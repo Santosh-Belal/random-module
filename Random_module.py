73fd176f22fa9328ea36f16e8238f38af7970916
Santosh Belal <107342749+Santosh-Belal@users.noreply.github.com>
2022-11-08 19:24:04 +0545

    Create Random_module.py

---
+++ Random_module.py
@@ -0,0 +1,40 @@
+santosh = random.random()*200
+
+print (santosh)
+my_tuple=("Apple","Ball","Cat","Dog","Eleplant","  ")
+
+my_choice=random.choice(my_tuple)
+
+print(my_choice
+
+import pyfiglet
+
+a=pyfiglet.figlet_format("Guess")
+
+b=pyfiglet.figlet_format("the number")
+
+print(a,b)
+
+import random
+
+x=random.randint(1,10)
+
+if x==7 :
+
+    print("Hurray !! you ar winner" + "\nYour random number ",x)
+
+elif (x>5) or (x<7) :
+
+    print("You got 10 !!" + " \nYour random number ",x)
+
+elif x>8  or x<10:
+
+    print('You got 9'+" \nYour random number ",x)
+
+else:
+
+    print("lol"+" \nYour random number ",x)
+
+
+
+
